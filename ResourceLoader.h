fa2e9d2d7acf96fcddaf13b53c70b2598cdda59f
Tyler-JP <tylerjaramillopr@ufl.edu>
2023-08-16 02:09:12 -0400

    Removed duplicate files

--- ResourceLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "raylib.h"
-#include <string>
-
-class ResourceLoader {
-public:
-	static Texture2D LoadTextures(const std::string& filePath);
-	static void UnloadTexture(Texture2D texture);
-};
